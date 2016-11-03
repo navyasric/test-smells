0e14dade6b64b7d811fdae6159a8f1c10cd0ff3a
Navyasri <ncanumalla@pivotal.io>
2016-11-03 12:28:31 -0700

    Break long test into small readable tests

--- smells/unclear/long/long.rb
+++ smells/unclear/long/long.rb
@@ -23,10 +23,47 @@ end
 
 # Test
 class Long < SmellTest
-  def test_make_sure_everything_works
+  # def test_make_sure_everything_works
+  #   subject = Stack.new
+  #
+  #   # Test Push
+  #   subject.push("A")
+  #   subject.push("B")
+  #   subject.push("C")
+  #
+  #   assert_equal "C", subject.pop
+  #   assert_equal "B", subject.pop
+  #   assert_equal "A", subject.pop
+  #
+  #   # Test Peek
+  #   subject.push("D")
+  #   subject.push("E")
+  #
+  #   assert_equal "E", subject.peek
+  #
+  #   subject.pop
+  #
+  #   assert_equal "D", subject.peek
+  #
+  #   subject.pop
+  #
+  #   # Test Depth
+  #   subject.push("F")
+  #   subject.push("G")
+  #
+  #   assert_equal 2, subject.depth
+  #
+  #   # Test Pop
+  #   subject.pop
+  #   subject.pop
+  #
+  #   assert_equal 0, subject.depth
+  #   assert_equal nil, subject.pop
+  # end
+
+  def test_push
     subject = Stack.new
 
-    # Test Push
     subject.push("A")
     subject.push("B")
     subject.push("C")
@@ -34,30 +71,38 @@ class Long < SmellTest
     assert_equal "C", subject.pop
     assert_equal "B", subject.pop
     assert_equal "A", subject.pop
+  end
 
-    # Test Peek
+  def test_peek
+    subject = Stack.new
+
     subject.push("D")
     subject.push("E")
 
     assert_equal "E", subject.peek
+  end
 
-    subject.pop
+  def test_peek_empty_stack
+    subject = Stack.new
+    assert_equal nil, subject.peek
+  end
 
-    assert_equal "D", subject.peek
+  def test_pop
+    subject = Stack.new
 
-    subject.pop
-
-    # Test Depth
-    subject.push("F")
-    subject.push("G")
+    subject.push("D")
+    subject.push("E")
 
     assert_equal 2, subject.depth
 
-    # Test Pop
-    subject.pop
-    subject.pop
-
+    assert_equal "E", subject.pop
+    assert_equal "D", subject.pop
     assert_equal 0, subject.depth
+  end
+
+  def test_pop_when_empty_stack
+    subject = Stack.new
     assert_equal nil, subject.pop
   end
+
 end
