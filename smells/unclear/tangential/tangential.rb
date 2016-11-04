411886b118ba71540873c2b85d3ace682e93e9c3
Navyasri <ncanumalla@pivotal.io>
2016-11-04 12:31:45 -0700

    Extracted phonevalidator object and tests.

--- smells/unclear/tangential/tangential.rb
+++ smells/unclear/tangential/tangential.rb
@@ -7,18 +7,24 @@ def set_attr(obj, name, value, type = nil)
     when :string
       valid = value.kind_of?(String)
     when :phone
-      north_american = /^\(?([2-9][0-9]{2})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
-      japanese = /^0\d{2}-\d{4}-\d{4}$/
-      intl_japanese = /^011-81-\d{2}-\d{4}-\d{4}$/
-      valid = value.match(north_american) ||
-        value.match(japanese) ||
-        value.match(intl_japanese)
+      valid = PhoneValidator.validate(value)
     end
 
     raise "#{value} is not a #{type}" unless valid
   end
 
   obj[name] = value
+end
+
+class PhoneValidator
+  def self.validate(phone)
+    north_american = /^\(?([2-9][0-9]{2})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+    japanese = /^0\d{2}-\d{4}-\d{4}$/
+    intl_japanese = /^011-81-\d{2}-\d{4}-\d{4}$/
+    phone.match(north_american) ||
+    phone.match(japanese) ||
+    phone.match(intl_japanese)
+  end
 end
 
 # Test
@@ -50,50 +56,36 @@ class Tangential < SmellTest
 
   def test_phone_type_correct
     user = OpenStruct.new
+    PhoneValidator.stub(:validate, true, ["(614) 349-4279"]) do
+      set_attr(user, :mobile, "(614) 349-4279", :phone)
 
-    set_attr(user, :mobile, "(614) 349-4279", :phone)
-
-    assert_equal "(614) 349-4279", user.mobile
+      assert_equal "(614) 349-4279", user.mobile
+    end
   end
 
   def test_phone_type_incorrect
     user = OpenStruct.new
-
-    error = assert_raises {
-      set_attr(user, :mobile, "1337", :phone)
-    }
-    assert_equal "1337 is not a phone", error.message
+    PhoneValidator.stub(:validate, false, ["1337"]) do
+      error = assert_raises {
+        set_attr(user, :mobile, "1337", :phone)
+      }
+      assert_equal "1337 is not a phone", error.message
+    end
   end
 
   def test_invalid_first_phone_character_cannot_start_with_1
-    user = OpenStruct.new
-
-    assert_raises {
-      set_attr(user, :mobile, "(123) 456-7890", :phone)
-    }
+    assert_nil PhoneValidator.validate("(123) 456-7890")
   end
 
   def test_simple_japanese_phone_number
-    user = OpenStruct.new
-
-    set_attr(user, :mobile, "090-1790-1357", :phone)
-
-    assert_equal "090-1790-1357", user.mobile
+    assert PhoneValidator.validate("090-1790-1357") != nil
   end
 
   def test_japanese_without_the_trunk
-    user = OpenStruct.new
-
-    assert_raises {
-      set_attr(user, :mobile, "90-1790-1357", :phone)
-    }
+    assert_nil PhoneValidator.validate("90-1790-1357")
   end
 
   def test_international_japanese_phone_number
-    user = OpenStruct.new
-
-    set_attr(user, :mobile, "011-81-90-1790-1357", :phone)
-
-    assert_equal "011-81-90-1790-1357", user.mobile
+    assert PhoneValidator.validate("011-81-90-1790-1357") != nil
   end
 end
